2138f95c813a1b1f95c617cf7e7c3678d477fcbf
James Bell <jbell303@gmail.com>
2018-12-05 14:14:26 -0800

    add ball

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    Rigidbody rb;
+    public float hitForce;
+    public Camera ARCamera;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 hitAngle = ARCamera.transform.forward;
+            Debug.Log("hit angle: " + hitAngle);
+            rb.AddForce(hitAngle * hitForce);
+        }
+	}
+}
