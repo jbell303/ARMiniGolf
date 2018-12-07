c4ff93389ea29a53ac55f7f74c8ef1cbb0ec330c
James Bell <jbell303@gmail.com>
2018-12-06 21:07:03 -0800

    build iOS

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -13,16 +13,18 @@ public class BallScript : MonoBehaviour {
 
     private void Start()
     {
-        ball = GameObject.FindGameObjectWithTag("ball");
         slider = Object.FindObjectOfType<Slider>();
         hitForce = 0;
     }
     public void HitBall ()
     {
-        
+    	ball = GameObject.FindGameObjectWithTag("ball");
         hitForce = slider.value;
+        Debug.Log("hitForce: " + hitForce);
         Vector3 hitAngle = ARCamera.transform.forward;
+        Debug.Log("hitAngle: "+ hitAngle);
         Vector3 force = hitAngle * hitForce * maxForce;
+        Debug.Log("ball: " + ball);
         ball.GetComponent<Rigidbody>().AddForce(force);
         Debug.Log("Force: " + force);
     }
