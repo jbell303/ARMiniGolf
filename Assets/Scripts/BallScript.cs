9034b795372c8be57604b3c8ee1a1ee81b6d01be
James Bell <jbell303@gmail.com>
2018-12-06 18:58:20 -0800

    update prefab, update UI

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,26 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour {
 
-    Rigidbody rb;
-    public float hitForce;
-    Camera ARCamera;
+    GameObject ball;
+    public Camera ARCamera;
+    float hitForce;
+    Slider slider;
+    public int maxForce;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-        ARCamera = Camera.main;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 hitAngle = ARCamera.transform.forward;
-            Debug.Log("hit angle: " + hitAngle);
-            rb.AddForce(hitAngle * hitForce);
-        }
-	}
+    private void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("ball");
+        slider = Object.FindObjectOfType<Slider>();
+        hitForce = 0;
+    }
+    public void HitBall ()
+    {
+        
+        hitForce = slider.value;
+        Vector3 hitAngle = ARCamera.transform.forward;
+        Vector3 force = hitAngle * hitForce * maxForce;
+        ball.GetComponent<Rigidbody>().AddForce(force);
+        Debug.Log("Force: " + force);
+    }
 }
